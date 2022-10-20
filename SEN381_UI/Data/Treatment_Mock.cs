8983a723618438e6e92163d3af186eb1664cc1a2
JFrederickK <576723@student.belgiumcampus.ac.za>
2022-10-20 11:13:22 +0200

    Added mock data

---
+++ SEN381_UI/Data/Treatment_Mock.cs
@@ -0,0 +1,17 @@
+﻿using SEN381_API_Group3.shared.models;
+
+namespace SEN381_UI.Data
+{
+    public class Treatment_Mock
+    {
+        public List<Treatment> GetTreatments() { 
+        List<Treatment> result = new List<Treatment>();
+
+            result.Add(new Treatment("1", "temp1", "desctiption", "1"));
+            result.Add(new Treatment("2", "temp2", "desctiption", "1"));
+            result.Add(new Treatment("3", "temp3", "desctiption", "2"));
+            result.Add(new Treatment("4", "temp4", "desctiption", "5"));
+            return result;
+        }
+    }
+}
