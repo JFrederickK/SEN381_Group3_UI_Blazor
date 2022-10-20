5ce32cc70e66580d3d151767a8595c5b334dfe40
JFrederickK <576723@student.belgiumcampus.ac.za>
2022-10-20 12:06:49 +0200

    Coverage klaar, Client en claim mock data ingesit.

--- SEN381_UI/Data/Claim.cs
+++ SEN381_UI/Data/Claim.cs
@@ -1,13 +1,59 @@
-﻿namespace SEN381_API_Group3.shared.models;
-
-public class Claim
+﻿namespace SEN381_UI.Data
 {
+    public class Claim
+    {
+        public string claimID;
+        public string client;
+        public string medicalConditions;
+        public string placeOfTreatment;
+        public string? callDetails = null;
+        public string claimeStatus;
 
-    public string ClaimID { get; set ; }
-    public string Client { get ; set ; }
-    public string MedicalConditions { get ; set ; }
-    public string PlaceOfTreatment { get ; set ; }
-    public string? CallDetails { get; set ; }
-    public string ClaimeStatus { get ; set ; }
+        public Claim(string claimID, string client, string medicalConditions, string placeOfTreatment, string? callDetails, string claimeStatus)
+        {
+            this.claimID = claimID;
+            this.client = client;
+            this.medicalConditions = medicalConditions;
+            this.placeOfTreatment = placeOfTreatment;
+            this.callDetails = callDetails;
+            this.claimeStatus = claimeStatus;
+        }
 
+
+        public string ClaimID { get => claimID; set => claimID = value; }
+        public string Client { get => client; set => client = value; }
+        public string MedicalConditions { get => medicalConditions; set => medicalConditions = value; }
+        public string PlaceOfTreatment { get => placeOfTreatment; set => placeOfTreatment = value; }
+        public string? CallDetails { get => callDetails; set => callDetails = value; }
+        public string ClaimeStatus { get => claimeStatus; set => claimeStatus = value; }
+
+
+        public Boolean isValid()//Make sure claim is valid
+        {
+            //Implement Logic
+            return true;
+        }
+
+        public void approveClaim()
+        {
+            //Implement Logic
+        }
+
+        public void rejectClaim()
+        {
+            //Implement Logic
+        }
+
+        public void pendAwaitingClaimForm()//When Claim needs to use a form instead of the new system
+        {
+            //Implement Logic
+        }
+
+        public void sendConfirmation()//Send sms or email confirmation to client
+        {
+            //Implement Logic
+        }
+
+    }
 }
+
---
+++ SEN381_UI/Data/ClientMock.cs
@@ -0,0 +1,13 @@
+﻿using SEN381_UI.Data;
+
+namespace SEN381_UI.Data
+{
+    public class ClientMock
+    {
+        public List<Client> GetClients() {
+        List<Client> clients = new List<Client>();
+            clients.Add(new Client("1", "Jandre", "Kruger","27Sonneveld", "jandreKruger01@gmail.com", "0786077697", "1", "Approved", "needed"));
+            return clients;
+        }
+    }
+}
