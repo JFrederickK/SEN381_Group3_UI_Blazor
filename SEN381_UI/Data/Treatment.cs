36a842061e4fe5a658f636cea9bc1834df86f1a3
JFrederickK <576723@student.belgiumcampus.ac.za>
2022-10-20 10:54:14 +0200

    Treatment klaar

--- SEN381_UI/Data/Treatment.cs
+++ SEN381_UI/Data/Treatment.cs
@@ -1,7 +1,22 @@
-﻿namespace SEN381_UI.Data
-{
-    public class Treatment
-    {
+﻿namespace SEN381_API_Group3.shared.models;
 
+public class Treatment
+{
+    string treatmentID;
+    string treatmentName;
+    string treatmentDescription;
+    string medicalServiceProviders;
+
+    public Treatment(string treatmentID, string treatmentName, string treatmentDescription, string medicalServiceProviders)
+    {
+        this.treatmentID = treatmentID;
+        this.treatmentName = treatmentName;
+        this.treatmentDescription = treatmentDescription;
+        this.medicalServiceProviders = medicalServiceProviders;
     }
+
+    public string TreatmentID { get => treatmentID; set => treatmentID = value; }
+    public string TreatmentName { get => treatmentName; set => treatmentName = value; }
+    public string TreatmentDescription { get => treatmentDescription; set => treatmentDescription = value; }
+    public string MedicalServiceProviders { get => medicalServiceProviders; set => medicalServiceProviders = value; }
 }
