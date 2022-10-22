6348aa38803fec75f938ddef62d397fa30e32d0b
JFrederickK <576723@student.belgiumcampus.ac.za>
2022-10-22 13:03:16 +0200

    Om nuwe claim te add is reg. Die dummy data vir die medical condtions en treatments lyk nie goed nie, dis et om te wys

--- SEN381_UI/Data/Client.cs
+++ SEN381_UI/Data/Client.cs
@@ -2,17 +2,45 @@
 
 public class Client
 {
+    string clientID;
+    string clientName;
+    string clientSurname;
+    string clientAddress;
+    string clientEmail;
+    string clientPhoneNumber;
+    string policies;
+    string clientStatus;
+    string clientAdHocNotes;
 
+    public Client()
+    {
+    }
 
+    public Client(string clientID, string clientName, string clientSurname, string clientAddress, string clientEmail, string clientPhoneNumber, string policies, string clientStatus, string clientAdHocNotes)
+    {
+        this.clientID = clientID;
+        this.clientName = clientName;
+        this.clientSurname = clientSurname;
+        this.clientAddress = clientAddress;
+        this.clientEmail = clientEmail;
+        this.clientPhoneNumber = clientPhoneNumber;
+        this.policies = policies;
+        this.clientStatus = clientStatus;
+        this.clientAdHocNotes = clientAdHocNotes;
+    }
 
-    public string ClientID { get; set; }
-    public string ClientName { get; set; }
-    public string ClientSurname { get; set; }
-    public string ClientAddress { get; set; }
-    public string ClientEmail { get; set; }
-    public string ClientPhoneNumber { get; set; }
-    public string Policies { get; set; }
-    public string ClientStatus { get; set; }
-    public string ClientAdHocNotes { get; set; }
+    public string ClientID { get => clientID; set => clientID = value; }
+    public string ClientName { get => clientName; set => clientName = value; }
+    public string ClientSurname { get => clientSurname; set => clientSurname = value; }
+    public string ClientAddress { get => clientAddress; set => clientAddress = value; }
+    public string ClientEmail { get => clientEmail; set => clientEmail = value; }
+    public string ClientPhoneNumber { get => clientPhoneNumber; set => clientPhoneNumber = value; }
+    public string Policies { get => policies; set => policies = value; }
+    public string ClientStatus { get => clientStatus; set => clientStatus = value; }
+    public string ClientAdHocNotes { get => clientAdHocNotes; set => clientAdHocNotes = value; }
 
+    public void makeClaim()
+    {
+        //Implement Logic
+    }
 }
---
+++ SEN381_UI/Data/ClientMock.cs
@@ -0,0 +1,20 @@
+﻿using SEN381_API_Group3.shared.models;
+
+namespace SEN381_UI.Data
+{
+    public class ClientMock
+    {
+        public List<Client> GetClients() { 
+        List<Client> clients = new List<Client>();
+            clients.Add(new Client("1", "Jandre","Kruger", "27 sonnevled", "Jandre@gmail", "1234567891", "1", "Approved", "notes"));
+            clients.Add(new Client("1", "Jandre", "Kruger", "27 sonnevled", "Jandre@gmail", "1234567891", "1", "Approved", "notes"));
+            clients.Add(new Client("1", "Jandre", "Kruger", "27 sonnevled", "Jandre@gmail", "1234567891", "1", "Approved", "notes"));
+            clients.Add(new Client("1", "Jandre", "Kruger", "27 sonnevled", "Jandre@gmail", "1234567891", "1", "Approved", "notes"));
+            clients.Add(new Client("1", "Jandre", "Kruger", "27 sonnevled", "Jandre@gmail", "1234567891", "1", "Approved", "notes"));
+            clients.Add(new Client("1", "Jandre", "Kruger", "27 sonnevled", "Jandre@gmail", "1234567891", "1", "Approved", "notes"));
+            clients.Add(new Client("1", "Jandre", "Kruger", "27 sonnevled", "Jandre@gmail", "1234567891", "1", "Approved", "notes"));
+
+            return clients;
+        }
+    }
+}
