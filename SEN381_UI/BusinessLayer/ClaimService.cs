01b94f222e980cf3cf0b4f65a9d082d57bc8b674
JFrederickK <576723@student.belgiumcampus.ac.za>
2022-10-26 16:19:44 +0200

    Ek het baie goed verander, die client UI, en het post probeer maar dit werk nie, help sal waardeer word

---
+++ SEN381_UI/BusinessLayer/ClaimService.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using SEN381_UI.Data;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+
+namespace SEN381_UI.BusinessLayer
+{
+    public class ClaimService
+    {
+
+            public async Task<List<Claim>> LoadClaim()
+            {
+                using var claim = new HttpClient();
+
+                var result = await claim.GetAsync("https://localhost:7116/api/Claim?page=1&size=1");
+
+
+                var resp = await result.Content.ReadAsStringAsync();
+                List<Claim> contributors = JsonConvert.DeserializeObject<List<Claim>>(resp);
+                return contributors;
+
+            }
+        
+    }
+}
---
+++ SEN381_UI/BusinessLayer/ClientService.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+using SEN381_UI.Data;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+
+
+namespace SEN381_UI.BusinessLayer
+{
+    public class ClientService
+    {
+        public async Task<List<Client>> LoadClient()
+        { 
+        using var client = new HttpClient();
+
+            var result = await client.GetAsync("https://localhost:7116/api/Client?page=1&size=3");
+
+
+            var resp = await result.Content.ReadAsStringAsync();
+            List<Client> contributors = JsonConvert.DeserializeObject<List<Client>>(resp);
+            return contributors;
+            
+        }
+
+        public async Task postClientDetails(Client person) {
+
+            var json = JsonConvert.SerializeObject(person);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            var url = "https://localhost:7116/api/Client";
+            using var client = new HttpClient();
+            var response = await client.PostAsync(url, data);
+            var result = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(result);
+
+        }
+
+    }
+}
