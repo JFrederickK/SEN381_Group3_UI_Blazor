032eb27e70194f961cda4a7e6db467534175dac6
DeRaven <stoltz278@gmail.com>
2022-10-27 09:30:28 +0200

    added post and list get

---
+++ SEN381_UI/BusinessLayer/Services/ClientService.cs
@@ -0,0 +1,15 @@
+﻿namespace SEN381_UI.BusinessLayer.Services
+{
+    public class ClientService
+    {
+        public async void getClientList()
+        {
+            var client = new HttpClient();
+
+            var result = await client.GetAsync("https://localhost:7116/api/Client");
+            Console.WriteLine("Client:");
+            Console.WriteLine(result.Content.ReadAsStream());
+            
+        }
+    }
+}
---
+++ SEN381_UI/BusinessLayer/Services/CoverageService.cs
@@ -0,0 +1,36 @@
+﻿using SEN381_UI.Data;
+using Newtonsoft.Json;
+using System;
+
+namespace SEN381_UI.BusinessLayer.Services
+{
+    public class CoverageService
+    {
+
+        public async Task<List<Coverage>> getCoverages(int size, int page)
+        {
+            var client = new HttpClient();
+            string url = $"https://localhost:7116/api/Coverage?page={page}&size={size}";
+            HttpResponseMessage response = await client.GetAsync(url);
+            List<Coverage> coverages= JsonConvert.DeserializeObject<List<Coverage>>(await response.Content.ReadAsStringAsync());
+            if (coverages !=null)
+            {
+                Console.WriteLine(coverages);
+                return coverages;
+            }
+            return new List<Coverage>();
+        }
+
+        public async Task<Coverage> addCoverage(Coverage cov)
+        {
+            var client = new HttpClient();
+            string url = $"https://localhost:7116/api/Coverage";
+            HttpResponseMessage response = await client.PostAsJsonAsync(url,cov);
+            Coverage coverage = JsonConvert.DeserializeObject<Coverage>(await response.Content.ReadAsStringAsync());
+
+            Console.WriteLine(coverage);
+            return coverage;
+
+        }
+    }
+}
