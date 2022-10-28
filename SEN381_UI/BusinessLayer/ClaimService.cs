fbef0657c5343570cab504820815998fbe7a5d94
JFrederickK <576723@student.belgiumcampus.ac.za>
2022-10-28 11:08:13 +0200

    Client post en claim get en client get

--- SEN381_UI/BusinessLayer/ClaimService.cs
+++ SEN381_UI/BusinessLayer/ClaimService.cs
@@ -8,18 +8,17 @@ namespace SEN381_UI.BusinessLayer
     public class ClaimService
     {
 
-            public async Task<List<Claim>> LoadClaim()
-            {
-                using var claim = new HttpClient();
+        public async Task<List<Claim>> LoadClaim()
+        {
+            var claim = new HttpClient();
 
-                var result = await claim.GetAsync("https://localhost:7116/api/Claim?page=1&size=1");
+            var result = await claim.GetAsync($"https://localhost:7116/api/Claims?page=1&size=2");
+            var resp = await result.Content.ReadAsStringAsync();
+            List<Claim> contributors = JsonConvert.DeserializeObject<List<Claim>>(resp);
+            Console.WriteLine(contributors);
+            return contributors;
 
+        }
 
-                var resp = await result.Content.ReadAsStringAsync();
-                List<Claim> contributors = JsonConvert.DeserializeObject<List<Claim>>(resp);
-                return contributors;
-
-            }
-        
     }
 }
--- SEN381_UI/BusinessLayer/ClientService.cs
+++ SEN381_UI/BusinessLayer/ClientService.cs
@@ -3,7 +3,7 @@ using System.Text;
 using SEN381_UI.Data;
 using System.Net.Http.Json;
 using System.Text.Json.Serialization;
-
+using System.Net;
 
 namespace SEN381_UI.BusinessLayer
 {
@@ -13,24 +13,21 @@ namespace SEN381_UI.BusinessLayer
         { 
         using var client = new HttpClient();
 
-            var result = await client.GetAsync("https://localhost:7116/api/Client?page=1&size=3");
-
-
+            var result = await client.GetAsync("https://localhost:7116/api/Client?page=1&size=5");
             var resp = await result.Content.ReadAsStringAsync();
             List<Client> contributors = JsonConvert.DeserializeObject<List<Client>>(resp);
             return contributors;
             
         }
 
-        public async Task postClientDetails(Client person) {
-
-            var json = JsonConvert.SerializeObject(person);
-            var data = new StringContent(json, Encoding.UTF8, "application/json");
-            var url = "https://localhost:7116/api/Client";
-            using var client = new HttpClient();
-            var response = await client.PostAsync(url, data);
-            var result = await response.Content.ReadAsStringAsync();
-            Console.WriteLine(result);
+        public async Task<Client> postClientDetails(Client person) {
+            HttpClient client = new HttpClient();
+            string url = $"https://localhost:7116/api/Client";
+            HttpResponseMessage response = await client.PostAsJsonAsync(url, person);
+            Client postclient = JsonConvert.DeserializeObject<Client>(await response.Content.ReadAsStringAsync());
+            Console.WriteLine("clinet info");
+            Console.WriteLine(postclient.clientAddress);
+            return postclient;
 
         }
 
