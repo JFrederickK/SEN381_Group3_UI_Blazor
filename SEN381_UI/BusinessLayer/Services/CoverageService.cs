b63c1cfec7c963f0c2469d6e7c4ce761383a98d6
DeRaven <stoltz278@gmail.com>
2022-10-30 12:02:22 +0200

    Added Coverage By ID service

--- SEN381_UI/BusinessLayer/Services/CoverageService.cs
+++ SEN381_UI/BusinessLayer/Services/CoverageService.cs
@@ -9,13 +9,12 @@ namespace SEN381_UI.BusinessLayer.Services
 
         public async Task<List<Coverage>> getCoverages(int size, int page)
         {
-            var client = new HttpClient();
+            HttpClient client = new HttpClient();
             string url = $"https://localhost:7116/api/Coverage?page={page}&size={size}";
             HttpResponseMessage response = await client.GetAsync(url);
             List<Coverage> coverages= JsonConvert.DeserializeObject<List<Coverage>>(await response.Content.ReadAsStringAsync());
             if (coverages !=null)
             {
-                Console.WriteLine(coverages);
                 return coverages;
             }
             return new List<Coverage>();
@@ -23,7 +22,7 @@ namespace SEN381_UI.BusinessLayer.Services
 
         public async Task<Coverage> addCoverage(Coverage cov)
         {
-            var client = new HttpClient();
+            HttpClient client = new HttpClient();
             string url = $"https://localhost:7116/api/Coverage";
             HttpResponseMessage response = await client.PostAsJsonAsync(url,cov);
             Coverage coverage = JsonConvert.DeserializeObject<Coverage>(await response.Content.ReadAsStringAsync());
@@ -35,7 +34,8 @@ namespace SEN381_UI.BusinessLayer.Services
 
         public async Task<Coverage> getCoverageDetails(string id)
         {
-            var client = new HttpClient();
+            Console.WriteLine("Start Request");
+            HttpClient client = new HttpClient();
             string url = $"https://localhost:7116/api/Coverage/{id}";
             HttpResponseMessage response = await client.GetAsync(url);
             Coverage coverage = JsonConvert.DeserializeObject<Coverage>(await response.Content.ReadAsStringAsync());
@@ -49,7 +49,7 @@ namespace SEN381_UI.BusinessLayer.Services
 
         public async Task<Coverage> deleteCoverage(string id)
         {
-            var client = new HttpClient();
+            HttpClient client = new HttpClient();
             string url = $"https://localhost:7116/api/Coverage/{id}";
             HttpResponseMessage response = await client.DeleteAsync(url);
             Coverage coverage = JsonConvert.DeserializeObject<Coverage>(await response.Content.ReadAsStringAsync());
@@ -61,7 +61,7 @@ namespace SEN381_UI.BusinessLayer.Services
 
         public async Task<Coverage> updateCoverage(Coverage cov)
         {
-            var client = new HttpClient();
+            HttpClient client = new HttpClient();
             string url = $"https://localhost:7116/api/Coverage/{cov.CoverageID}";
             HttpResponseMessage response = await client.PutAsJsonAsync<Coverage>(url,cov);
             Coverage coverage = JsonConvert.DeserializeObject<Coverage>(await response.Content.ReadAsStringAsync());
