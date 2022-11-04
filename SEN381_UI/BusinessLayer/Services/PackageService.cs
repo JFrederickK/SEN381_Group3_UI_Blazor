cc219d1c2f04d2bb02bd4f808dff805bca8b7be7
DeRaven <stoltz278@gmail.com>
2022-11-04 11:15:56 +0200

    Added package services

---
+++ SEN381_UI/BusinessLayer/Services/PackageService.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using SEN381_UI.Data;
+
+namespace SEN381_UI.BusinessLayer.Services
+{
+    public class PackageService
+    {
+        public async Task<List<Package>> getPackages(int size, int page)
+        {
+            HttpClient client = new HttpClient();
+            string url = $"https://localhost:7116/api/Package?page={page}&size={size}";
+            HttpResponseMessage response = await client.GetAsync(url);
+            List<Package> packages = JsonConvert.DeserializeObject<List<Package>>(await response.Content.ReadAsStringAsync());
+            if (packages != null)
+            {
+                return packages;
+            }
+            return new List<Package>();
+        }
+
+        public async Task<Package> addPackage(Package package)
+        {
+            HttpClient client = new HttpClient();
+            string url = $"https://localhost:7116/api/Package";
+            HttpResponseMessage response = await client.PostAsJsonAsync<Package>(url, package);
+            Package pack = JsonConvert.DeserializeObject<Package>(await response.Content.ReadAsStringAsync());
+
+            return pack;
+
+        }
+
+        public async Task<Package> getPacakgeDetails(string id)
+        {
+            HttpClient client = new HttpClient();
+            string url = $"https://localhost:7116/api/Package/{id}";
+            HttpResponseMessage response = await client.GetAsync(url);
+            Package package = JsonConvert.DeserializeObject<Package>(await response.Content.ReadAsStringAsync());
+            if (package != null)
+            {
+                return package;
+            }
+            return null;
+        }
+
+        public async Task<Package> deletePackage(string id)
+        {
+            HttpClient client = new HttpClient();
+            string url = $"https://localhost:7116/api/Package/{id}";
+            HttpResponseMessage response = await client.DeleteAsync(url);
+            Package pack = JsonConvert.DeserializeObject<Package>(await response.Content.ReadAsStringAsync());
+
+            return pack;
+
+        }
+
+        public async Task<Package> updatePackage(Package package)
+        {
+            HttpClient client = new HttpClient();
+            string url = $"https://localhost:7116/api/Package/{package.PackageID}";
+            HttpResponseMessage response = await client.PutAsJsonAsync<Package>(url, package);
+            Package pack = JsonConvert.DeserializeObject<Package>(await response.Content.ReadAsStringAsync());
+
+            return pack;
+
+        }
+    }
+}
--- SEN381_UI/Data/Package.cs
+++ SEN381_UI/Data/Package.cs
@@ -5,17 +5,18 @@ namespace SEN381_UI.Data;
 public class Package
     {
         string packageID;
-        List<TreatmentCoverage> treatmentCoverages;
+        List<PackageTreatmentCoverage> treatmentCoverages;
 
     public Package()
     {}
 
-    public Package(string packageID, List<TreatmentCoverage> treatmentCoverages)
+    public Package(string packageID, List<PackageTreatmentCoverage> treatmentCoverages)
     {
         this.packageID = packageID;
         this.treatmentCoverages = treatmentCoverages;
     }
 
     public string PackageID { get => packageID; set => packageID = value; }
-    public List<TreatmentCoverage> TreatmentCoverages { get => treatmentCoverages; set => treatmentCoverages = value; }
+    public List<PackageTreatmentCoverage> TreatmentCoverages { get => treatmentCoverages; set => treatmentCoverages = value; }
 }
+
