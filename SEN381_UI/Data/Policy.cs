ae1ea8fc0f8dac854cc3bdbe4330f8ceb1da476c
WernerPret <wernerpretorius333@gmail.com>
2022-11-07 21:09:56 +0200

    added date range to model

--- SEN381_UI/Data/Policy.cs
+++ SEN381_UI/Data/Policy.cs
@@ -1,17 +1,21 @@
-﻿namespace SEN381_UI.Data;
+﻿using MudBlazor;
+
+namespace SEN381_UI.Data;
 
 public class Policy
 {
     private string policyId = "";
     private string policyName = "";
     private string policyStatus = "";
+    public DateRange dateAvailable;
     private List<Package> package = new List<Package>();
 
-    public Policy(string policyId, string policyName, string policyStatus, List<Package> package)
+    public Policy(string policyId, string policyName, string policyStatus, DateRange dateAvailable, List<Package> package)
     {
         PolicyId = policyId;
         PolicyName = policyName;
         PolicyStatus = policyStatus;
+        dateAvailable = dateAvailable;
         Package = package;
     }
     public Policy()
@@ -22,5 +26,8 @@ public class Policy
     public string PolicyId { get => policyId; set => policyId = value; }
     public string PolicyName { get => policyName; set => policyName = value; }
     public string PolicyStatus { get => policyStatus; set => policyStatus = value; }
+    
+    public DateRange DateAvailable { get => dateAvailable; set => dateAvailable = value; }
+
     public List<Package> Package { get => package; set => package = value; }
 }
