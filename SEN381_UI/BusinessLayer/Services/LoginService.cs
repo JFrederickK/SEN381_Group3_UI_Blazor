1723615d1179792f7249fbf8a3a8ccb1b335260d
DeRaven <stoltz278@gmail.com>
2022-11-12 11:07:46 +0200

    Finished login

--- SEN381_UI/BusinessLayer/Services/LoginService.cs
+++ SEN381_UI/BusinessLayer/Services/LoginService.cs
@@ -5,53 +5,24 @@ namespace SEN381_UI.BusinessLayer.Services
 {
     public class LoginService
     {
-        public async Task<bool> LoadLogin()
+        public async Task<EmployeeDetails?> getLoginByEmail(string email, string password, string name)
         {
-            using var medical = new HttpClient();
-
-            var result = await medical.GetAsync("https://localhost:7116/api/LoginReg");
-            var resp = await result.Content.ReadAsStringAsync();
-            bool contributors = JsonConvert.DeserializeObject<bool>(resp);
-            return contributors;
-
+            HttpClient client = new HttpClient();
+            string url = $"https://localhost:7116/api/LoginReg/Login?email={email}&password={password}&name={name}";
+            url = url.Replace("@","%40");
+            HttpResponseMessage response = await client.GetAsync(url);
+            EmployeeDetails? emp = JsonConvert.DeserializeObject<EmployeeDetails?>(await response.Content.ReadAsStringAsync());
+            Console.WriteLine(emp);
+            return emp;
         }
-        public async Task<bool> getLoginByEmail()
-        {
-            Console.WriteLine("Start Request");
-            HttpClient medical = new HttpClient();
-            string url = $"https://localhost:7116/api/LoginReg/{employee}";
-            HttpResponseMessage response = await medical.GetAsync(url);
-            bool newmedical = JsonConvert.DeserializeObject<bool>(await response.Content.ReadAsStringAsync());
-            return newmedical;
-        }
-       public async Task<EmployeeDetails> updateMedical(EmployeeDetails cov)
-        {
-            Console.WriteLine("Update start");
-            var medical = new HttpClient();
-            string url = $"https://localhost:7116/api/LoginReg/{cov.EmployeeEmail}";
-            HttpResponseMessage response = await medical.PutAsJsonAsync(url, cov);
-            EmployeeDetails coverage = JsonConvert.DeserializeObject<EmployeeDetails>(await response.Content.ReadAsStringAsync());
-            Console.WriteLine(coverage);
-            return coverage;
-
-        }
-        public async Task<EmployeeDetails> postEmployee(EmployeeDetails person)
+        public async Task<EmployeeDetails?> postEmployee(EmployeeDetails person)
         {
             var medical = new HttpClient();
             string url = $"https://localhost:7116/api/LoginReg/";
             HttpResponseMessage response = await medical.PostAsJsonAsync(url, person);
-            EmployeeDetails postclient = JsonConvert.DeserializeObject<EmployeeDetails>(await response.Content.ReadAsStringAsync());
-            return postclient;
+            EmployeeDetails? postEmp = JsonConvert.DeserializeObject<EmployeeDetails?>(await response.Content.ReadAsStringAsync());
+            return postEmp;
 
         }
-        public async Task<bool> deleteMedicalCondition(string id)
-        {
-            var medical = new HttpClient();
-            string url = $"https://localhost:7116/api/LoginReg/{id}";
-            HttpResponseMessage response = await medical.DeleteAsync(url);
-            bool coverage = JsonConvert.DeserializeObject<bool>(await response.Content.ReadAsStringAsync());
-            return coverage;
-        }
-
     }
 }
