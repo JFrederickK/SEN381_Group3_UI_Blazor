eb7aface940a62e8db2147d859efac062d25acaa
JFrederickK <576723@student.belgiumcampus.ac.za>
2022-11-12 12:44:11 +0200

    Client & family

--- SEN381_UI/Data/FamilyMember.cs
+++ SEN381_UI/Data/FamilyMember.cs
@@ -1,4 +1,6 @@
 
+using System.ComponentModel.DataAnnotations;
+
 public class FamilyMember{
 
     private string memberId;
@@ -26,11 +28,26 @@ public class FamilyMember{
     }
 
     public string MemberId { get => memberId; set => memberId = value; }
+    [Required]
+    [StringLength(50, ErrorMessage = "Name length can't be more than 50.")]
+    [RegularExpression(@"^[a-zA-Z]{2,}$", ErrorMessage = "The Name contains invalid characters.")]
     public string MemberName { get => memberName; set => memberName = value; }
+    [Required]
+    [StringLength(50, ErrorMessage = "Name length can't be more than 50.")]
+    [RegularExpression(@"^[a-zA-Z]{2,}$", ErrorMessage = " Surename contains invalid characters.")]
     public string MemberSurname { get => memberSurname; set => memberSurname = value; }
+    [Required]
+    [RegularExpression(@"^([+]?\d{1,2}[-\s]?|)\d{3}[-\s]?\d{3}[-\s]?\d{4}$", ErrorMessage = "Invalid phone number.")]
     public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+    [Required]
+    [EmailAddress]
     public string Email { get => email; set => email = value; }
+    [Required]
+    [RegularExpression(@"^[0-9]+\s[a-zA-Z\s\-']{2,}.\s?[a-zA-Z\s\(\),]{2,}$", ErrorMessage = "Wrong address format.")]
     public string Address { get => address; set => address = value; }
+    [Required]
+    [StringLength(50, ErrorMessage = "Role length can't be more than 50.")]
+    [RegularExpression(@"^[a-zA-Z]{2,}$", ErrorMessage = "Role contains invalid characters.")]
     public string Role { get => role; set => role = value; }
     public string ClientID { get => clientID; set => clientID = value; }
 }
---
+++ SEN381_UI/Data/Validation.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SEN381_UI.Data
+{
+    public class Validation
+    {
+        public bool StringOnly(string word) {
+            var Inputletters = @"^[a-zA-Z]{2,}$";
+            bool val = Regex.IsMatch(word, Inputletters);
+
+        return val;
+        }
+        public bool numberOnlu(string word) {
+            var Inputletter = @"^([+]?\d{1,2}[-\s]?|)\d{3}[-\s]?\d{3}[-\s]?\d{4}$";
+            bool val = Regex.IsMatch(word, Inputletter);
+            return val;
+        }
+        public bool AddressFormat(string word) {
+            var Inputletter = @"^[0-9]+\s[a-zA-Z\s\-']{2,}.\s?[a-zA-Z\s\(\),]{2,}$";
+            bool val = Regex.IsMatch(word, Inputletter);
+            return val;
+        }
+        public bool EmailFormat(string word) { 
+        var Inputletter = @"/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|
+        \\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|
+        \[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:
+        (?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/";
+            bool val = Regex.IsMatch(word,Inputletter);
+            return val;
+        }
+    }
+}
