2a496960b0541a790998a8254f1914a4e5c268a5
JFrederickK <576723@student.belgiumcampus.ac.za>
2022-11-13 11:58:17 +0200

    Medical done

--- SEN381_UI/Data/InsertMedicalConditionRequest.cs
+++ SEN381_UI/Data/InsertMedicalConditionRequest.cs
@@ -1,4 +1,6 @@
-﻿namespace SEN381_UI.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SEN381_UI.Data
 {
     public class InsertMedicalConditionRequest
     {
@@ -17,8 +19,12 @@
             IDs1 = iDs;
             this.Counts = counts;
         }
-
+        [Required]
+        [StringLength(50, ErrorMessage = "Name length can't be more than 50.")]
+        [RegularExpression(@"^[a-zA-Z]{2,}$", ErrorMessage = "The Name contains invalid characters.")]
         public string Name { get => name; set => name = value; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Description length can't be more than 100.")]
         public string Description { get => description; set => description = value; }
         public List<string> IDs1 { get => IDs; set => IDs = value; }
         public int Counts { get => counts; set => counts = value; }
